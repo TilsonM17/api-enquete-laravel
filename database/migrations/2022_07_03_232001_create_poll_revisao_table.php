a3e03a239b874abf8185d676f598a3bf4a6d1dee
TilsonM17 <tilsonmat@gmail.com>
2022-07-04 00:22:21 +0100

    Alterar a estrutura das tabelas da base de Dados

---
+++ database/migrations/2022_07_03_232001_create_poll_revisao_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('poll', function (Blueprint $table) {
+            $table->integer("views")->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('poll_revisao');
+    }
+};
